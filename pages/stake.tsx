b5ac464221af5991b2d2e54e1be6ed26fe2e0c42
Maxium from YDM <iamliqiang@gmail.com>
2023-07-19 16:36:45 +0800

    btn

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -109,8 +109,8 @@ const Stake: NextPage = () => {
         <>
           <h2 className={styles.h2}>Your $DLD Token</h2>
           <h5 className={styles.h5}>$DLD Address : 0x866f63535993887A78234e16fE3c88298E81Ac1c</h5>
-          <button className="mybtn" onClick={importToken}>
-            [Import $DLD]
+          <button className="button" onClick={importToken}>
+            Import $DLD
           </button>
 
           <div className={styles.tokenGrid}>
